31081b1796801865e8339f57e957e8605363ad57
Gambit1011 <tokkiecj@gmail.com>
2021-08-29 08:50:58 +0200

    Test Git

--- newBooking.Designer.cs
+++ newBooking.Designer.cs
@@ -29,7 +29,17 @@ namespace vaalrusGUIPrototype
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "test";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // newBooking
             // 
@@ -37,6 +47,7 @@ namespace vaalrusGUIPrototype
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(219)))), ((int)(((byte)(168)))));
             this.ClientSize = new System.Drawing.Size(745, 467);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "newBooking";
             this.Text = "newBooking";
@@ -45,5 +56,7 @@ namespace vaalrusGUIPrototype
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
