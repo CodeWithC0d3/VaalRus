acf20248f72b314d4dba18c6e205e47f4ed74752
SirClasen <90693487+SirClasen@users.noreply.github.com>
2021-09-17 16:35:22 +0200

    set up of initial data for database
    set up of initial data for database, in one query

---
+++ _SQL/Initial Data Population.sql
@@ -0,0 +1,50 @@
+INSERT INTO Customer
+VALUES 
+('Jimmy', 'Hendrix', '6500000000001', 'jimHendrix@email.com', '0000000000', '62 Valley street'),
+('Gary', 'Puckett', '6200000000002', 'GaryPuckett@email.com', '0000000000', '52 Round street'),
+('Tina', 'Turner', '6100000000003', 'TinaTurner@email.com', '0000000000', '41 Frank street');
+
+
+
+
+
+
+
+
+
+
+INSERT INTO Accommodationtype
+VALUES 
+(0), -- caravan / camp
+(1); -- chalet
+
+
+INSERT INTO Accommodation
+VALUES 
+-- caravan / camp
+(0, 5, 1110.0), 
+(0, 5, 1110.0),
+(0, 5, 1110.0),
+(0, 5, 1110.0),
+(0, 5, 1110.0),
+(0, 5, 1110.0),
+(0, 5, 1110.0),
+(0, 5, 1110.0),
+(0, 5, 1110.0),
+(0, 5, 1110.0),
+-- chalet
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0),
+(1, 8, 2500.0); 
