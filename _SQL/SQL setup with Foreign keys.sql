fcd9b1c5103191520073c9e70357ea196a994507
SirClasen <90693487+SirClasen@users.noreply.github.com>
2021-09-17 21:13:55 +0200

    DB structure update
    DB structure update

--- _SQL/SQL setup with Foreign keys.sql
+++ _SQL/SQL setup with Foreign keys.sql
@@ -1,25 +1,30 @@
 --CREATE DATABASE Vaalrus; 
 
--- CANT DROP TABLES LIKE THIS BECUASE OF FOREIGN KEYs
--- DROP TABLE IF EXISTS Customer;
--- DROP TABLE IF EXISTS Quotatio;
--- DROP TABLE IF EXISTS Booking;
--- DROP TABLE IF EXISTS Quotationstatus;
--- DROP TABLE IF EXISTS Payment;
--- DROP TABLE IF EXISTS Accommodationtype;
--- DROP TABLE IF EXISTS Accommodation;
--- DROP TABLE IF EXISTS Accommodationset;
-
+--CANT DROP TABLES LIKE THIS BECUASE OF FOREIGN KEYs
+DROP TABLE IF EXISTS Accommodationset;
+DROP TABLE IF EXISTS Accommodation;
+DROP TABLE IF EXISTS Accommodationtype;
+DROP TABLE IF EXISTS Payment;
+DROP TABLE IF EXISTS Booking;
+DROP TABLE IF EXISTS Quotation;
+DROP TABLE IF EXISTS Quotationstatus;
+DROP TABLE IF EXISTS Customer;
 
 
 CREATE TABLE Customer (
     Customer_ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
     Customer_FirstName VARCHAR(50) NOT NULL,
     Customer_LastName VARCHAR(50),
-	Customner_IDNumber CHAR(13) NOT NULL,
-	Customer_Email VARCHAR(50) NOT NULL UNIQUE,
-	Customner_Cell VARCHAR(15) NOT NULL,
-    Customer_Address VARCHAR(50) -- Address seems to be a key word in SQL
+	Customer_IDNumber CHAR(13) NOT NULL,
+	Customer_Email VARCHAR(50) UNIQUE,
+	Customer_Cell VARCHAR(15) NOT NULL,
+    Customer_Address NVARCHAR(250) -- Address seems to be a keyword in SQL
+
+);
+
+CREATE TABLE Quotationstatus (
+    Status_ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+    Status_Type VARCHAR(20)
 
 );
 
@@ -28,10 +33,10 @@ CREATE TABLE Quotation (
     Customer_ID INT NOT NULL REFERENCES Customer(Customer_ID), -- FK
     Reservation_Date DATE NOT NULL,
 	Duration INT NOT NULL,
-	TotalPrice DECIMAL,
-	PaymentStatus BIT DEFAULT 0, -- Status seems to be a key word in SQL
-    QuoteCreated_DateTime DATETIME,
-	CreatedBy VARCHAR(50)
+	TotalPrice SMALLMONEY NOT NULL,
+	PaymentStatus INT NOT NULL REFERENCES Quotationstatus(Status_ID), -- Status seems to be a keyword in SQL
+    QuoteCreated_DateTime DATETIME NOT NULL,
+	CreatedBy VARCHAR(50) NOT NULL
 
 );
 
@@ -46,23 +51,17 @@ CREATE TABLE Booking (
 
 );
 
-CREATE TABLE Quotationstatus (
-    Status_ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-    Status_Type tinyint DEFAULT 0 --- leave as tinint (3 values: 0 pending, 1 accepted, 2 rejected)
-
-);
-
 CREATE TABLE Payment (
     Payment_ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Quotation_ID INT NOT NULL REFERENCES Quotation(Quotation_ID), -- FK
-    ReceivedAmount DECIMAL, -- SQL has a currency option, wasn't sure what to go with
+    ReceivedAmount SMALLMONEY, -- SQL has a currency option, wasn't sure what to go with
     Payment_Date DATETIME,
 	Payment_Status BIT DEFAULT 0
 );
 
 CREATE TABLE Accommodationtype (
     Accommodation_TypeID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-	AccommodationType BIT, -- Type seems to be a key word in SQL
+	AccommodationType VARCHAR(20) -- Type seems to be a key word in SQL
 
 );
 
@@ -70,17 +69,18 @@ CREATE TABLE Accommodation (
 --what about a name of each place???
     Accommodation_ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
 	Accommodation_TypeID INT NOT NULL REFERENCES Accommodationtype(Accommodation_TypeID), -- FK
-	Number_Of_occupants INT,
-	Accommodation_Price DECIMAL -- SQL has a currency option, wasn't sure what to go with
+	Number_Of_Occupants INT NOT NULL,
+	Accommodation_Price SMALLMONEY NOT NULL -- SQL has a currency option, wasn't sure what to go with
 
 );
 
 
-
 CREATE TABLE Accommodationset (
-    Quotation_ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY, --FK also
+    Quotation_ID INT NOT NULL PRIMARY KEY REFERENCES Quotation(Quotation_ID),
+	--Quotation_ID INT NOT NULL REFERENCES Quotation(Quotation_ID), --FK also
     Accommodation_ID INT NOT NULL REFERENCES Accommodation(Accommodation_ID), --FK
-	Number_Of_Occupants INT
+	Number_Of_Occupants INT,
+	--CONSTRAINT PK_QuoteID FOREIGN KEY (Quotation_ID) REFERENCES Quotation(Quotation_ID)
 
 );
 
