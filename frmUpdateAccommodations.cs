6257988db1cef08be460fb31c13be18c70dd3468
Gambit1011 <tokkiecj@gmail.com>
2021-09-18 13:57:58 +0200

    FixError

---
+++ frmUpdateAccommodations.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vaalrusGUIPrototype
+{
+    public partial class frmUpdateAccommodations : Form
+    {
+        public frmUpdateAccommodations()
+        {
+            InitializeComponent();
+        }
+        private void LoadTheme()
+        {
+            foreach (Control btns in this.Controls)
+            {
+                foreach (Control pnl in this.Controls)
+                {
+                    if (pnl.GetType() == typeof(Panel))
+                    {
+                        pnl.Parent = picBackground;
+                        pnl.BackColor = Color.Transparent;
+                    }
+                }
+                foreach (Control lbls in this.Controls)
+                {
+                    if (lbls.GetType() == typeof(Label))
+                    {
+                        Label lbl = (Label)lbls;
+                        //lbl.Font = GlobalSettings.font;
+                        lbl.Parent = picBackground;
+                        lbl.ForeColor = GlobalSettings.SecondaryColor;
+                        lbl.BackColor = Color.Transparent;
+
+                    }
+                }               
+
+            }
+
+            aplytheme(pnlConnetionControls);
+            aplytheme(pnlGroupBoxDetails);
+            aplytheme2(pnlGroupBoxSearch);
+            aplytheme2(pnlGroupBoxDetails2);
+            //aplytheme(pnl_device);
+            //aplytheme(pnl_main);
+
+        }
+        private void aplytheme(Control co)
+        {
+            if (co.GetType() == typeof(Panel))
+            {
+                foreach (Control btns in co.Controls)
+                {
+                    if (btns.GetType() == typeof(Button))
+                    {
+                        Button btn = (Button)btns;
+                        btn.FlatStyle = FlatStyle.Flat;
+                        //btn.Parent = pictureBox1;
+                        btn.BackColor = GlobalSettings.PrimaryColor;
+                        btn.ForeColor = Color.White;
+                        btn.Font = GlobalSettings.font;
+                        //btn.FlatAppearance.BorderColor = ThemeColor.SecondaryColor;
+                        btn.FlatAppearance.BorderColor = Color.White;
+                        btn.FlatAppearance.BorderSize = 2;
+
+                    }
+                }
+                foreach (Control lbls in co.Controls)
+                {
+                    if (lbls.GetType() == typeof(Label))
+                    {
+                        Label lbl = (Label)lbls;
+                        lbl.Font = GlobalSettings.font;
+                        //lbl.Parent = picBackground;
+                        lbl.ForeColor = GlobalSettings.SecondaryColor;
+                        lbl.BackColor = Color.Transparent;
+
+                    }
+                }
+                foreach (Control dtg in co.Controls)
+                {
+                    if (dtg.GetType() == typeof(DataGridView))
+                    {
+                        DataGridView dtgg = (DataGridView)dtg;
+                        dtgg.ForeColor = Color.White;
+                        dtgg.BackgroundColor = SystemColors.Control;
+                        dtgg.DefaultCellStyle.BackColor = GlobalSettings.PrimaryColor;
+                        dtgg.DefaultCellStyle.Font = new Font("Arial", float.Parse("10"), FontStyle.Regular);
+
+                    }
+                }
+                foreach (Control chk in co.Controls)
+                {
+                    if (chk.GetType() == typeof(CheckBox))
+                    {
+                        CheckBox chkk = (CheckBox)chk;
+                        chkk.ForeColor = GlobalSettings.SecondaryColor;
+
+                    }
+                }
+                foreach (Control txt in co.Controls)
+                {
+                    if (txt.GetType() == typeof(TextBox))
+                    {
+                        TextBox txtC = (TextBox)txt;
+                        txtC.ForeColor = GlobalSettings.SecondaryColor;
+
+                    }
+                }
+                foreach (Control grb in co.Controls)
+                {
+                    if (grb.GetType() == typeof(GroupBox))
+                    {
+                        GroupBox txtC = (GroupBox)grb;
+                        txtC.ForeColor = GlobalSettings.SecondaryColor;
+                        txtC.Font = GlobalSettings.font;
+                    }
+                }
+            }
+        }
+        private void aplytheme2(Control co)
+        {
+            if (co.GetType() == typeof(Panel))
+            {
+                foreach (Control btns in co.Controls)
+                {
+                    if (btns.GetType() == typeof(Button))
+                    {
+                        Button btn = (Button)btns;
+                        btn.FlatStyle = FlatStyle.Flat;
+                        //btn.Parent = pictureBox1;
+                        btn.BackColor = GlobalSettings.PrimaryColor;
+                        btn.ForeColor = Color.White;
+                        btn.Font = GlobalSettings.font;
+                        //btn.FlatAppearance.BorderColor = ThemeColor.SecondaryColor;
+                        btn.FlatAppearance.BorderColor = Color.White;
+                        btn.FlatAppearance.BorderSize = 2;
+
+                    }
+                }
+                foreach (Control lbls in co.Controls)
+                {
+                    if (lbls.GetType() == typeof(Label))
+                    {
+                        Label lbl = (Label)lbls;
+                        lbl.Font = GlobalSettings.font;
+                        //lbl.Parent = picBackground;
+                        //lbl.ForeColor = GlobalSettings.SecondaryColor;
+                        lbl.ForeColor = GlobalSettings.thirdColor;
+                        lbl.BackColor = Color.Transparent;
+
+                    }
+                }
+                foreach (Control dtg in co.Controls)
+                {
+                    if (dtg.GetType() == typeof(DataGridView))
+                    {
+                        DataGridView dtgg = (DataGridView)dtg;
+                        dtgg.ForeColor = Color.White;
+                        dtgg.BackgroundColor = SystemColors.Control;
+                        dtgg.DefaultCellStyle.BackColor = GlobalSettings.PrimaryColor;
+                        dtgg.DefaultCellStyle.Font = new Font("Arial", float.Parse("10"), FontStyle.Regular);
+
+                    }
+                }
+                foreach (Control chk in co.Controls)
+                {
+                    if (chk.GetType() == typeof(CheckBox))
+                    {
+                        CheckBox chkk = (CheckBox)chk;
+                        chkk.ForeColor = GlobalSettings.SecondaryColor;
+
+                    }
+                }
+                foreach (Control txt in co.Controls)
+                {
+                    if (txt.GetType() == typeof(TextBox))
+                    {
+                        TextBox txtC = (TextBox)txt;
+                        txtC.ForeColor = GlobalSettings.SecondaryColor;
+
+                    }
+                }
+                foreach (Control grb in co.Controls)
+                {
+                    if (grb.GetType() == typeof(GroupBox))
+                    {
+                        GroupBox txtC = (GroupBox)grb;
+                        txtC.ForeColor = GlobalSettings.thirdColor;
+                        txtC.Font = GlobalSettings.font;
+                    }
+                }
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmUpdateAccommodations_Load(object sender, EventArgs e)
+        {
+            LoadTheme();
+        }
+    }
+}
