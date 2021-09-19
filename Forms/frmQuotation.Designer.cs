e40a04f3271079a99d9e8bbc12a919d69a40a30c
Gambit1011 <tokkiecj@gmail.com>
2021-09-19 17:49:49 +0200

    Added populate script

---
+++ Forms/frmQuotation.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace vaalrusGUIPrototype.Forms
+{
+    partial class frmQuotation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBackground = new System.Windows.Forms.PictureBox();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpReservation = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBackground)).BeginInit();
+            this.pnlMain.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picBackground
+            // 
+            this.picBackground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picBackground.Image = global::vaalrusGUIPrototype.Properties.Resources.backgroundImage;
+            this.picBackground.Location = new System.Drawing.Point(0, 0);
+            this.picBackground.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picBackground.Name = "picBackground";
+            this.picBackground.Size = new System.Drawing.Size(1067, 554);
+            this.picBackground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBackground.TabIndex = 0;
+            this.picBackground.TabStop = false;
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Controls.Add(this.groupBox1);
+            this.pnlMain.Controls.Add(this.label2);
+            this.pnlMain.Controls.Add(this.comboBox1);
+            this.pnlMain.Controls.Add(this.label1);
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(1067, 554);
+            this.pnlMain.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(425, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Customer - ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(514, 33);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(184, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(769, 33);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Reservation Date";
+            // 
+            // dtpReservation
+            // 
+            this.dtpReservation.Location = new System.Drawing.Point(54, 23);
+            this.dtpReservation.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpReservation.Name = "dtpReservation";
+            this.dtpReservation.Size = new System.Drawing.Size(265, 23);
+            this.dtpReservation.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 28);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "From";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 59);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "To";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(54, 54);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 23);
+            this.dateTimePicker1.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.dtpReservation);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(49, 85);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(943, 380);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Booking Details";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(9, 226);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(214, 148);
+            this.listBox1.TabIndex = 7;
+            // 
+            // frmQuotation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.picBackground);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmQuotation";
+            this.Text = "frmQuotation";
+            this.Load += new System.EventHandler(this.frmQuotation_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBackground)).EndInit();
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBackground;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpReservation;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Forms/frmQuotation.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vaalrusGUIPrototype.Forms
+{
+    public partial class frmQuotation : Form
+    {
+        public frmQuotation()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadTheme()
+        {
+            foreach (Control btns in this.Controls)
+            {
+                foreach (Control pnl in this.Controls)
+                {
+                    if (pnl.GetType() == typeof(Panel))
+                    {
+                        pnl.Parent = picBackground;
+                        pnl.BackColor = Color.Transparent;
+                    }
+                }
+                foreach (Control lbls in this.Controls)
+                {
+                    if (lbls.GetType() == typeof(Label))
+                    {
+                        Label lbl = (Label)lbls;
+                        //lbl.Font = GlobalSettings.font;
+                        lbl.Parent = picBackground;
+                        lbl.ForeColor = GlobalSettings.SecondaryColor;
+                        lbl.BackColor = Color.Transparent;
+
+                    }                   
+                }
+
+
+            }            
+            aplytheme(pnlMain);
+            //aplytheme(pnl_main);
+
+        }
+        private void aplytheme(Control co)
+        {
+            if (co.GetType() == typeof(Panel))
+            {
+                foreach (Control btns in co.Controls)
+                {
+                    if (btns.GetType() == typeof(Button))
+                    {
+                        Button btn = (Button)btns;
+                        btn.FlatStyle = FlatStyle.Flat;
+                        //btn.Parent = pictureBox1;
+                        btn.BackColor = GlobalSettings.PrimaryColor;
+                        btn.ForeColor = Color.White;
+                        btn.Font = GlobalSettings.font;
+                        //btn.FlatAppearance.BorderColor = ThemeColor.SecondaryColor;
+                        btn.FlatAppearance.BorderColor = Color.White;
+                        btn.FlatAppearance.BorderSize = 2;
+
+                    }
+                }
+                foreach (Control lbls in co.Controls)
+                {
+                    if (lbls.GetType() == typeof(Label))
+                    {
+                        Label lbl = (Label)lbls;
+                        lbl.Font = GlobalSettings.font;
+                        //lbl.Parent = picBackground;
+                        lbl.ForeColor = GlobalSettings.SecondaryColor;
+                        lbl.BackColor = Color.Transparent;
+
+                    }
+                }
+                foreach (Control dtg in co.Controls)
+                {
+                    if (dtg.GetType() == typeof(DataGridView))
+                    {
+                        DataGridView dtgg = (DataGridView)dtg;
+                        dtgg.ForeColor = Color.White;
+                        dtgg.BackgroundColor = SystemColors.Control;
+                        dtgg.DefaultCellStyle.BackColor = GlobalSettings.PrimaryColor;
+                        dtgg.DefaultCellStyle.Font = new Font("Arial", float.Parse("10"), FontStyle.Regular);
+
+                    }
+                }
+                foreach (Control chk in co.Controls)
+                {
+                    if (chk.GetType() == typeof(CheckBox))
+                    {
+                        CheckBox chkk = (CheckBox)chk;
+                        chkk.ForeColor = GlobalSettings.SecondaryColor;
+
+                    }
+                }
+                foreach (Control txt in co.Controls)
+                {
+                    if (txt.GetType() == typeof(TextBox))
+                    {
+                        TextBox txtC = (TextBox)txt;
+                        txtC.ForeColor = GlobalSettings.SecondaryColor;
+
+                    }
+                }
+                foreach (Control cmb in co.Controls)
+                {
+                    if (cmb.GetType() == typeof(ComboBox))
+                    {
+                        TextBox cmbc = (TextBox)cmb;
+                        cmbc.ForeColor = GlobalSettings.SecondaryColor;
+                        cmbc.Font = GlobalSettings.font;
+
+                    }
+                }
+                foreach (Control dtp in co.Controls)
+                {
+                    if (dtp.GetType() == typeof(DateTimePicker))
+                    {
+                        TextBox dtpc = (TextBox)dtp;
+                        dtpc.ForeColor = GlobalSettings.SecondaryColor;
+                        dtpc.Font = GlobalSettings.font;
+
+                    }
+                }
+            }
+        }
+
+        private void frmQuotation_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
