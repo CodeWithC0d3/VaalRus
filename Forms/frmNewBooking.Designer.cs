6207950db9c7c9a78ee32fe4bdcf32d0eaf8e74a
Gambit1011 <tokkiecj@gmail.com>
2021-10-07 17:43:22 +0200

    newbooking

---
+++ Forms/frmNewBooking.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace vaalrusGUIPrototype.Forms
+{
+    partial class frmNewBooking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.pnlGrid = new System.Windows.Forms.Panel();
+            this.pnlFilter = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pnlGrid.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Location = new System.Drawing.Point(8, 17);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(354, 545);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // pnlGrid
+            // 
+            this.pnlGrid.Controls.Add(this.dataGridView1);
+            this.pnlGrid.Controls.Add(this.pnlFilter);
+            this.pnlGrid.Location = new System.Drawing.Point(385, 17);
+            this.pnlGrid.Name = "pnlGrid";
+            this.pnlGrid.Size = new System.Drawing.Size(657, 545);
+            this.pnlGrid.TabIndex = 1;
+            // 
+            // pnlFilter
+            // 
+            this.pnlFilter.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlFilter.Location = new System.Drawing.Point(0, 0);
+            this.pnlFilter.Name = "pnlFilter";
+            this.pnlFilter.Size = new System.Drawing.Size(657, 75);
+            this.pnlFilter.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 75);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(657, 470);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // frmNewBooking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::vaalrusGUIPrototype.Properties.Resources.backgroundImage;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1054, 575);
+            this.Controls.Add(this.pnlGrid);
+            this.Controls.Add(this.pnlMain);
+            this.Name = "frmNewBooking";
+            this.Text = "frmNewBooking";
+            this.Load += new System.EventHandler(this.frmNewBooking_Load);
+            this.pnlGrid.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Panel pnlGrid;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel pnlFilter;
+    }
+}
---
+++ Forms/frmNewBooking.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Reporting.WinForms;
+using System.Drawing.Printing;
+
+namespace vaalrusGUIPrototype.Forms
+{
+    public partial class frmNewBooking : Form
+    {
+        string constr = Properties.Settings.Default.conString;
+        SqlConnection con;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        SqlDataReader dataReader;
+        DataSet ds;
+        string sql = "";
+
+        /*private ErrorProvider userErrorProvider;
+        private ErrorProvider customerErrorProvider;
+        private ErrorProvider fromDateErrorProvider;
+        private ErrorProvider toDateErrorProvider;
+        private ErrorProvider typeErrorProvider;
+        private ErrorProvider accListErrorProvider;*/
+        public frmNewBooking()
+        {
+            InitializeComponent();
+        }
+        private void LoadTheme()
+        {
+            foreach (Control co in this.Controls)
+            {
+
+                // if (co.GetType() == typeof(Panel))
+                // {
+                //    co.Parent = this;
+                //   co.BackColor = Color.FromArgb(58, 93, 117);
+                //}
+                if (co.GetType() == typeof(Label))
+                {
+                    Label lbl = (Label)co;
+                    lbl.Font = GlobalSettings.font;
+                    lbl.Font = new Font("Microsoft Sans Serif", 12);
+                    lbl.Parent = this;
+                    lbl.ForeColor = GlobalSettings.SecondaryColor;
+                    lbl.BackColor = Color.Transparent;
+
+                }
+                if (co.GetType() == typeof(Button))
+                {
+                    Button btn = (Button)co;
+                    btn.FlatStyle = FlatStyle.Flat;
+                    //btn.Parent = pictureBox1;
+                    btn.BackColor = GlobalSettings.PrimaryColor;
+                    btn.ForeColor = Color.White;
+                    btn.Font = GlobalSettings.font;
+                    //btn.FlatAppearance.BorderColor = ThemeColor.SecondaryColor;
+                    btn.FlatAppearance.BorderColor = Color.White;
+                    btn.FlatAppearance.BorderSize = 2;
+                }
+            }
+            //pnlMain.BackColor = Color.Transparent;
+
+            aplytheme(pnlMain);
+            aplytheme(pnlFilter);
+            aplytheme(pnlGrid);
+            //aplytheme(pnl_accSet);
+            //timer1.Start();
+        }
+        private void aplytheme(Control pn)
+        {
+            if (pn.GetType() == typeof(Panel))
+            {
+                pn.BackColor = Color.FromArgb(58, 93, 117);
+                foreach (Control co in pn.Controls)
+                {
+                    if (co.GetType() == typeof(Button))
+                    {
+                        Button btn = (Button)co;
+                        btn.FlatStyle = FlatStyle.Flat;
+                        //btn.Parent = pictureBox1;
+                        btn.BackColor = GlobalSettings.PrimaryColor;
+                        btn.ForeColor = Color.White;
+                        btn.Font = GlobalSettings.font;
+                        //btn.FlatAppearance.BorderColor = ThemeColor.SecondaryColor;
+                        btn.FlatAppearance.BorderColor = Color.White;
+                        btn.FlatAppearance.BorderSize = 2;
+                    }
+                    if (co.GetType() == typeof(Label))
+                    {
+                        Label lbl = (Label)co;
+                        lbl.Font = GlobalSettings.font;
+                        //lbl.Parent = picBackground;
+                        //lbl.ForeColor = GlobalSettings.SecondaryColor;
+                        lbl.ForeColor = Color.White;
+                        lbl.BackColor = Color.Transparent;
+
+                    }
+                    if (co.GetType() == typeof(DataGridView))
+                    {
+                        DataGridView dtgg = (DataGridView)co;
+                        dtgg.ForeColor = Color.White;
+                        dtgg.BackgroundColor = GlobalSettings.PrimaryColor;
+                        dtgg.DefaultCellStyle.BackColor = GlobalSettings.PrimaryColor;
+                        dtgg.DefaultCellStyle.Font = new Font("Arial", float.Parse("10"), FontStyle.Regular);
+                        dtgg.DefaultCellStyle.SelectionBackColor = GlobalSettings.ChangeColorBrightness(GlobalSettings.PrimaryColor, -0.2);
+                        dtgg.ColumnHeadersDefaultCellStyle.BackColor = GlobalSettings.ChangeColorBrightness(GlobalSettings.PrimaryColor, -0.2);
+                        dtgg.ColumnHeadersDefaultCellStyle.SelectionBackColor = GlobalSettings.ChangeColorBrightness(GlobalSettings.PrimaryColor, -0.2);
+                        dtgg.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+                        dtgg.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                        dtgg.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+
+                    }
+                    if (co.GetType() == typeof(CheckBox))
+                    {
+                        CheckBox chkk = (CheckBox)co;
+                        chkk.ForeColor = GlobalSettings.SecondaryColor;
+
+                    }
+                    if (co.GetType() == typeof(TextBox))
+                    {
+                        TextBox txtC = (TextBox)co;
+                        txtC.ForeColor = GlobalSettings.SecondaryColor;
+
+                    }
+                    if (co.GetType() == typeof(ComboBox))
+                    {
+                        ComboBox cmbc = (ComboBox)co;
+                        cmbc.ForeColor = GlobalSettings.SecondaryColor;
+                        cmbc.Font = GlobalSettings.font;
+
+                    }
+                    if (co.GetType() == typeof(DateTimePicker))
+                    {
+                        DateTimePicker dtpc = (DateTimePicker)co;
+                        dtpc.ForeColor = GlobalSettings.SecondaryColor;
+                        dtpc.Font = GlobalSettings.font;
+
+                    }
+                    if (co.GetType() == typeof(GroupBox))
+                    {
+                        GroupBox gpc = (GroupBox)co;
+                        gpc.ForeColor = GlobalSettings.SecondaryColor;
+                        gpc.Font = GlobalSettings.font;
+
+                    }
+                    if (co.GetType() == typeof(ListBox))
+                    {
+                        ListBox lsc = (ListBox)co;
+                        lsc.ForeColor = GlobalSettings.SecondaryColor;
+                        lsc.Font = GlobalSettings.font;
+
+                    }
+                }
+
+            }
+        }
+        private void frmNewBooking_Load(object sender, EventArgs e)
+        {
+            LoadTheme();
+        }
+        private void createErProviders()
+        {
+            /*userErrorProvider = new ErrorProvider();
+
+            userErrorProvider.SetIconAlignment(txtUser, ErrorIconAlignment.MiddleRight);
+            userErrorProvider.SetIconPadding(txtUser, 2);
+            userErrorProvider.BlinkRate = 1000;
+            userErrorProvider.BlinkStyle = ErrorBlinkStyle.AlwaysBlink;
+
+            customerErrorProvider = new ErrorProvider();
+            customerErrorProvider.SetIconAlignment(cmbCustomer, ErrorIconAlignment.MiddleRight);
+            customerErrorProvider.SetIconPadding(cmbCustomer, 2);
+            customerErrorProvider.BlinkStyle = ErrorBlinkStyle.BlinkIfDifferentError;
+
+            fromDateErrorProvider = new ErrorProvider();
+            fromDateErrorProvider.SetIconAlignment(dpFrom, ErrorIconAlignment.MiddleRight);
+            fromDateErrorProvider.SetIconPadding(dpFrom, 2);
+            fromDateErrorProvider.BlinkRate = 1000;
+            fromDateErrorProvider.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+
+            toDateErrorProvider = new ErrorProvider();
+            toDateErrorProvider.SetIconAlignment(dpTo, ErrorIconAlignment.MiddleRight);
+            toDateErrorProvider.SetIconPadding(dpTo, 2);
+            toDateErrorProvider.BlinkRate = 1000;
+            toDateErrorProvider.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+
+            typeErrorProvider = new ErrorProvider();
+            typeErrorProvider.SetIconAlignment(cmbFilter, ErrorIconAlignment.MiddleRight);
+            typeErrorProvider.SetIconPadding(cmbFilter, 2);
+            typeErrorProvider.BlinkRate = 1000;
+            typeErrorProvider.BlinkStyle = ErrorBlinkStyle.NeverBlink;
+
+            accListErrorProvider = new ErrorProvider();
+            accListErrorProvider.SetIconAlignment(lstAccommodation, ErrorIconAlignment.MiddleRight);
+            accListErrorProvider.SetIconPadding(lstAccommodation, 2);
+            accListErrorProvider.BlinkRate = 1000;
+            accListErrorProvider.BlinkStyle = ErrorBlinkStyle.NeverBlink;*/
+
+        }
+
+
+    }
+}
