0ae700d43c2cf02760e642f51726fb1f48160b28
JMX969 <jmx969@gmail.com>
2021-10-07 22:46:51 +0200

    up

--- frmReportBookings.cs
+++ frmReportBookings.cs
@@ -40,6 +40,132 @@ namespace vaalrusGUIPrototype
 
             con.Close();
         }*/
+        private void LoadTheme()
+        {
+            foreach (Control co in this.Controls)
+            {
+
+                // if (co.GetType() == typeof(Panel))
+                // {
+                //    co.Parent = this;
+                //    co.BackColor = Color.Transparent;
+                // }
+                if (co.GetType() == typeof(Label))
+                {
+                    Label lbl = (Label)co;
+                    lbl.Font = GlobalSettings.font;
+                    lbl.Font = new Font("Microsoft Sans Serif", 12);
+                    lbl.Parent = this;
+                    lbl.ForeColor = GlobalSettings.SecondaryColor;
+                    lbl.BackColor = Color.Transparent;
+
+                }
+                /*if (co.GetType() == typeof(Button))
+                {
+                    Button btn = (Button)co;
+                    btn.FlatStyle = FlatStyle.Flat;
+                    //btn.Parent = pictureBox1;
+                    btn.BackColor = GlobalSettings.PrimaryColor;
+                    btn.ForeColor = Color.White;
+                    btn.Font = GlobalSettings.font;
+                    //btn.FlatAppearance.BorderColor = ThemeColor.SecondaryColor;
+                    btn.FlatAppearance.BorderColor = Color.White;
+                    btn.FlatAppearance.BorderSize = 2;
+                }*/
+            }
+
+
+            aplytheme(panelInput);
+            
+        }
+        private void aplytheme(Control pn)
+        {
+            if (pn.GetType() == typeof(Panel))
+            {
+                foreach (Control co in pn.Controls)
+                {
+                    if (co.GetType() == typeof(Button))
+                    {
+                        Button btn = (Button)co;
+                        btn.FlatStyle = FlatStyle.Flat;
+                        //btn.Parent = pictureBox1;
+                        btn.BackColor = GlobalSettings.PrimaryColor;
+                        btn.ForeColor = Color.White;
+                        btn.Font = GlobalSettings.font;
+                        //btn.FlatAppearance.BorderColor = ThemeColor.SecondaryColor;
+                        btn.FlatAppearance.BorderColor = Color.White;
+                        btn.FlatAppearance.BorderSize = 2;
+                    }
+                    if (co.GetType() == typeof(Label))
+                    {
+                        Label lbl = (Label)co;
+                        lbl.Font = GlobalSettings.font;
+                        //lbl.Parent = picBackground;
+                        //lbl.ForeColor = GlobalSettings.SecondaryColor;
+                        lbl.ForeColor = Color.White;
+                        lbl.BackColor = Color.Transparent;
+
+                    }
+                    if (co.GetType() == typeof(DataGridView))
+                    {
+                        DataGridView dtgg = (DataGridView)co;
+                        dtgg.ForeColor = Color.White;
+                        dtgg.BackgroundColor = GlobalSettings.PrimaryColor;
+                        dtgg.DefaultCellStyle.BackColor = GlobalSettings.PrimaryColor;
+                        dtgg.DefaultCellStyle.Font = new Font("Arial", float.Parse("10"), FontStyle.Regular);
+                        dtgg.DefaultCellStyle.SelectionBackColor = GlobalSettings.ChangeColorBrightness(GlobalSettings.PrimaryColor, -0.2);
+                        dtgg.ColumnHeadersDefaultCellStyle.BackColor = GlobalSettings.ChangeColorBrightness(GlobalSettings.PrimaryColor, -0.2);
+                        dtgg.ColumnHeadersDefaultCellStyle.SelectionBackColor = GlobalSettings.ChangeColorBrightness(GlobalSettings.PrimaryColor, -0.2);
+                        dtgg.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+                        dtgg.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+                        dtgg.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+
+                    }
+                    if (co.GetType() == typeof(CheckBox))
+                    {
+                        CheckBox chkk = (CheckBox)co;
+                        chkk.ForeColor = GlobalSettings.SecondaryColor;
+
+                    }
+                    if (co.GetType() == typeof(TextBox))
+                    {
+                        TextBox txtC = (TextBox)co;
+                        txtC.ForeColor = GlobalSettings.SecondaryColor;
+
+                    }
+                    if (co.GetType() == typeof(ComboBox))
+                    {
+                        ComboBox cmbc = (ComboBox)co;
+                        cmbc.ForeColor = GlobalSettings.SecondaryColor;
+                        cmbc.Font = GlobalSettings.font;
+
+                    }
+                    if (co.GetType() == typeof(DateTimePicker))
+                    {
+                        DateTimePicker dtpc = (DateTimePicker)co;
+                        dtpc.ForeColor = GlobalSettings.SecondaryColor;
+                        dtpc.Font = GlobalSettings.font;
+
+                    }
+                    if (co.GetType() == typeof(GroupBox))
+                    {
+                        GroupBox gpc = (GroupBox)co;
+                        gpc.ForeColor = GlobalSettings.SecondaryColor;
+                        gpc.Font = GlobalSettings.font;
+
+                    }
+                    if (co.GetType() == typeof(ListBox))
+                    {
+                        ListBox lsc = (ListBox)co;
+                        lsc.ForeColor = GlobalSettings.SecondaryColor;
+                        lsc.Font = GlobalSettings.font;
+
+                    }
+                }
+
+            }
+        }
         private void frmReportBookings_Load(object sender, EventArgs e)
         {
             //DateTime year = new DateTime();
@@ -49,9 +175,10 @@ namespace vaalrusGUIPrototype
             //comboBoxYear.Items.Add(yearval);
             //for (int i = 0; i < 12; i++)
             //{
-                //comboBoxMonth.Items.Add(i);
+            //comboBoxMonth.Items.Add(i);
             //}
             //comboBoxYear
+            LoadTheme();
             this.reportViewer1.RefreshReport();
             
         }
@@ -122,7 +249,7 @@ namespace vaalrusGUIPrototype
                 StartDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1);
                 EndDate = new DateTime();
                 EndDate = DateTime.Today.AddMonths(1);
-                SqlCommand myComm4 = new SqlCommand("SELECT * FROM viewBookingReport where startDate >= '" + selectStart + "' AND startDate < '" + selectEnd + "' ;", conn);
+                SqlCommand myComm4 = new SqlCommand("SELECT * FROM viewBookingReport1 where startDate >= '" + selectStart + "' AND startDate < '" + selectEnd + "' ;", conn);
                 SqlDataAdapter adapter4 = new SqlDataAdapter(myComm4);
                 DataTable dt4 = new DataTable();
                 adapter4.Fill(dt4);
@@ -131,7 +258,7 @@ namespace vaalrusGUIPrototype
                 ReportDataSource rds3 = new ReportDataSource("DataSetBooking", dt);
                 ReportDataSource rds4 = new ReportDataSource("DataSetQ", dt2);
                 ReportDataSource rds5 = new ReportDataSource("DataSetASet", dt3);
-                ReportDataSource rds6 = new ReportDataSource("DataSet1", dt4);
+                ReportDataSource rds6 = new ReportDataSource("DataSetFull", dt4);
                 this.reportViewer1.LocalReport.DataSources.Add(rds3);
                 this.reportViewer1.LocalReport.DataSources.Add(rds4);
                 this.reportViewer1.LocalReport.DataSources.Add(rds5);
