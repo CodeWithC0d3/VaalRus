da6ac9548940fd9b3346613e1e505662548969f3
SirClasen <90693487+SirClasen@users.noreply.github.com>
2021-10-08 12:11:40 +0200

    prelim deleting query
    prelim deleting query

--- Forms/frmDeleteCustomer.Designer.cs
+++ Forms/frmDeleteCustomer.Designer.cs
@@ -29,84 +29,92 @@ namespace vaalrusGUIPrototype
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnAll = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblLastName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgView = new System.Windows.Forms.DataGridView();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnAll
             // 
-            this.button1.Location = new System.Drawing.Point(72, 21);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(212, 42);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Display ALL Customers";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAll.Location = new System.Drawing.Point(54, 17);
+            this.btnAll.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(159, 34);
+            this.btnAll.TabIndex = 0;
+            this.btnAll.Text = "Display ALL Customers";
+            this.btnAll.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnSearch
             // 
-            this.button2.Location = new System.Drawing.Point(77, 239);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(212, 50);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Search";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnSearch.Location = new System.Drawing.Point(58, 194);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(159, 41);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // btnDelete
             // 
-            this.button3.Location = new System.Drawing.Point(77, 356);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(212, 50);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Delete ";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnDelete.Location = new System.Drawing.Point(58, 289);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(159, 41);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete ";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.lblLastName);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.button3);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Location = new System.Drawing.Point(12, 25);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnAll);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Location = new System.Drawing.Point(9, 20);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(360, 451);
+            this.panel1.Size = new System.Drawing.Size(270, 366);
             this.panel1.TabIndex = 4;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
-            // textBox1
+            // lblLastName
             // 
-            this.textBox1.Location = new System.Drawing.Point(72, 150);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(212, 22);
-            this.textBox1.TabIndex = 5;
+            this.lblLastName.Location = new System.Drawing.Point(54, 122);
+            this.lblLastName.Margin = new System.Windows.Forms.Padding(2);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(160, 20);
+            this.lblLastName.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(69, 119);
+            this.label1.Location = new System.Drawing.Point(52, 97);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(210, 17);
+            this.label1.Size = new System.Drawing.Size(158, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Search customer by Last Name:";
             // 
-            // dataGridView1
+            // dgView
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(409, 25);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(599, 461);
-            this.dataGridView1.TabIndex = 0;
+            this.dgView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgView.Location = new System.Drawing.Point(307, 20);
+            this.dgView.Margin = new System.Windows.Forms.Padding(2);
+            this.dgView.Name = "dgView";
+            this.dgView.RowHeadersWidth = 51;
+            this.dgView.RowTemplate.Height = 24;
+            this.dgView.Size = new System.Drawing.Size(449, 375);
+            this.dgView.TabIndex = 0;
             // 
             // pictureBox1
             // 
@@ -114,26 +122,28 @@ namespace vaalrusGUIPrototype
             this.pictureBox1.Image = global::vaalrusGUIPrototype.Properties.Resources.backgroundImage;
             this.pictureBox1.InitialImage = null;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1020, 552);
+            this.pictureBox1.Size = new System.Drawing.Size(765, 448);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // frmDeleteCustomer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1020, 552);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(765, 448);
+            this.Controls.Add(this.dgView);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmDeleteCustomer";
             this.Text = "frmDeleteCustomer";
             this.Load += new System.EventHandler(this.frmDeleteCustomer_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -141,13 +151,13 @@ namespace vaalrusGUIPrototype
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnAll;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox lblLastName;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgView;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
