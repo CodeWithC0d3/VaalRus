c4ccfa350aeb2305e4485d4214dac60794538fd0
SirClasen <90693487+SirClasen@users.noreply.github.com>
2021-10-08 17:08:58 +0200

    delete customer fix ui
    delete customer fix ui

--- Forms/frmDeleteCustomer.Designer.cs
+++ Forms/frmDeleteCustomer.Designer.cs
@@ -35,17 +35,19 @@ namespace vaalrusGUIPrototype
             this.panel1 = new System.Windows.Forms.Panel();
             this.txtLastName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
             this.dgView = new System.Windows.Forms.DataGridView();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.lblOutput = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnAll
             // 
-            this.btnAll.Location = new System.Drawing.Point(21, 19);
+            this.btnAll.Location = new System.Drawing.Point(549, 9);
             this.btnAll.Margin = new System.Windows.Forms.Padding(2);
             this.btnAll.Name = "btnAll";
             this.btnAll.Size = new System.Drawing.Size(159, 34);
@@ -56,7 +58,7 @@ namespace vaalrusGUIPrototype
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(456, 92);
+            this.btnSearch.Location = new System.Drawing.Point(52, 74);
             this.btnSearch.Margin = new System.Windows.Forms.Padding(2);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(159, 41);
@@ -67,7 +69,7 @@ namespace vaalrusGUIPrototype
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(247, 181);
+            this.btnDelete.Location = new System.Drawing.Point(549, 74);
             this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(159, 41);
@@ -78,22 +80,20 @@ namespace vaalrusGUIPrototype
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.lblOutput);
             this.panel1.Controls.Add(this.txtLastName);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.btnDelete);
-            this.panel1.Controls.Add(this.btnAll);
             this.panel1.Controls.Add(this.btnSearch);
-            this.panel1.Location = new System.Drawing.Point(53, 268);
+            this.panel1.Location = new System.Drawing.Point(31, 337);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(665, 233);
+            this.panel1.Size = new System.Drawing.Size(721, 124);
             this.panel1.TabIndex = 4;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(247, 103);
+            this.txtLastName.Location = new System.Drawing.Point(52, 36);
             this.txtLastName.Margin = new System.Windows.Forms.Padding(2);
             this.txtLastName.Name = "txtLastName";
             this.txtLastName.Size = new System.Drawing.Size(160, 20);
@@ -103,12 +103,21 @@ namespace vaalrusGUIPrototype
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(61, 103);
+            this.label1.Location = new System.Drawing.Point(49, 12);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(119, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "Customer to be deleted:";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(49, 20);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(39, 13);
+            this.lblOutput.TabIndex = 6;
+            this.lblOutput.Text = "Output";
             // 
             // dgView
             // 
@@ -129,25 +138,26 @@ namespace vaalrusGUIPrototype
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(798, 501);
+            this.pictureBox1.Size = new System.Drawing.Size(759, 467);
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // lblOutput
+            // panel2
             // 
-            this.lblOutput.AutoSize = true;
-            this.lblOutput.Location = new System.Drawing.Point(77, 145);
-            this.lblOutput.Name = "lblOutput";
-            this.lblOutput.Size = new System.Drawing.Size(35, 13);
-            this.lblOutput.TabIndex = 6;
-            this.lblOutput.Text = "label2";
+            this.panel2.Controls.Add(this.lblOutput);
+            this.panel2.Controls.Add(this.btnAll);
+            this.panel2.Location = new System.Drawing.Point(31, 269);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(720, 52);
+            this.panel2.TabIndex = 6;
             // 
             // frmDeleteCustomer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(798, 501);
+            this.ClientSize = new System.Drawing.Size(759, 467);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.dgView);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
@@ -159,6 +169,8 @@ namespace vaalrusGUIPrototype
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -174,5 +186,6 @@ namespace vaalrusGUIPrototype
         private System.Windows.Forms.DataGridView dgView;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Panel panel2;
     }
 }
