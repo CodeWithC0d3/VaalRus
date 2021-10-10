62d812552c6468774dddc02b757d0b360f0b1c3e
Gambit1011 <tokkiecj@gmail.com>
2021-10-10 20:14:50 +0200

    DeleteAccDONE

--- Forms/frmDeleteAcommodation.Designer.cs
+++ Forms/frmDeleteAcommodation.Designer.cs
@@ -31,27 +31,24 @@ namespace vaalrusGUIPrototype.Forms
         private void InitializeComponent()
         {
             this.panelChildForm = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.lblSearchNumber = new System.Windows.Forms.Label();
-            this.btnDeleteAccomm = new System.Windows.Forms.Button();
+            this.pnlFilter = new System.Windows.Forms.Panel();
+            this.cmbType = new System.Windows.Forms.ComboBox();
             this.lblSearchType = new System.Windows.Forms.Label();
-            this.btnDisplayAccomm = new System.Windows.Forms.Button();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.dgView = new System.Windows.Forms.DataGridView();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.lblAccNr = new System.Windows.Forms.Label();
-            this.lblAccType = new System.Windows.Forms.Label();
-            this.lblNrOcc = new System.Windows.Forms.Label();
-            this.txtAccNr = new System.Windows.Forms.TextBox();
-            this.txtType = new System.Windows.Forms.TextBox();
+            this.pnlDelete = new System.Windows.Forms.Panel();
             this.txtNrOcc = new System.Windows.Forms.TextBox();
+            this.txtType = new System.Windows.Forms.TextBox();
+            this.txtAccNr = new System.Windows.Forms.TextBox();
+            this.lblNrOcc = new System.Windows.Forms.Label();
+            this.lblAccType = new System.Windows.Forms.Label();
+            this.btnDeleteAccomm = new System.Windows.Forms.Button();
+            this.lblAccNr = new System.Windows.Forms.Label();
+            this.pnlGrid = new System.Windows.Forms.Panel();
+            this.dgView = new System.Windows.Forms.DataGridView();
             this.panelChildForm.SuspendLayout();
-            this.panel1.SuspendLayout();
-            this.panel4.SuspendLayout();
+            this.pnlFilter.SuspendLayout();
+            this.pnlDelete.SuspendLayout();
+            this.pnlGrid.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgView)).BeginInit();
-            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panelChildForm
@@ -59,78 +56,141 @@ namespace vaalrusGUIPrototype.Forms
             this.panelChildForm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(152)))), ((int)(((byte)(219)))), ((int)(((byte)(168)))));
             this.panelChildForm.BackgroundImage = global::vaalrusGUIPrototype.Properties.Resources.backgroundImage;
             this.panelChildForm.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panelChildForm.Controls.Add(this.panel2);
-            this.panelChildForm.Controls.Add(this.panel1);
-            this.panelChildForm.Controls.Add(this.panel4);
+            this.panelChildForm.Controls.Add(this.pnlFilter);
+            this.panelChildForm.Controls.Add(this.pnlDelete);
+            this.panelChildForm.Controls.Add(this.pnlGrid);
             this.panelChildForm.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panelChildForm.Location = new System.Drawing.Point(0, 0);
-            this.panelChildForm.Margin = new System.Windows.Forms.Padding(4);
             this.panelChildForm.Name = "panelChildForm";
-            this.panelChildForm.Size = new System.Drawing.Size(1090, 553);
+            this.panelChildForm.Size = new System.Drawing.Size(818, 449);
             this.panelChildForm.TabIndex = 14;
             // 
-            // panel1
+            // pnlFilter
             // 
-            this.panel1.BackColor = System.Drawing.Color.Silver;
-            this.panel1.Controls.Add(this.txtNrOcc);
-            this.panel1.Controls.Add(this.txtType);
-            this.panel1.Controls.Add(this.txtAccNr);
-            this.panel1.Controls.Add(this.lblNrOcc);
-            this.panel1.Controls.Add(this.lblAccType);
-            this.panel1.Controls.Add(this.btnDeleteAccomm);
-            this.panel1.Controls.Add(this.lblAccNr);
-            this.panel1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.panel1.Location = new System.Drawing.Point(28, 26);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(296, 447);
-            this.panel1.TabIndex = 19;
+            this.pnlFilter.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlFilter.Controls.Add(this.cmbType);
+            this.pnlFilter.Controls.Add(this.lblSearchType);
+            this.pnlFilter.Location = new System.Drawing.Point(265, 21);
+            this.pnlFilter.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlFilter.Name = "pnlFilter";
+            this.pnlFilter.Size = new System.Drawing.Size(536, 24);
+            this.pnlFilter.TabIndex = 20;
             // 
-            // lblSearchNumber
+            // cmbType
             // 
-            this.lblSearchNumber.AutoSize = true;
-            this.lblSearchNumber.Location = new System.Drawing.Point(3, 66);
-            this.lblSearchNumber.Name = "lblSearchNumber";
-            this.lblSearchNumber.Size = new System.Drawing.Size(131, 17);
-            this.lblSearchNumber.TabIndex = 6;
-            this.lblSearchNumber.Text = "Accommodation Nr:";
-            // 
-            // btnDeleteAccomm
-            // 
-            this.btnDeleteAccomm.Location = new System.Drawing.Point(8, 138);
-            this.btnDeleteAccomm.Name = "btnDeleteAccomm";
-            this.btnDeleteAccomm.Size = new System.Drawing.Size(258, 58);
-            this.btnDeleteAccomm.TabIndex = 4;
-            this.btnDeleteAccomm.Text = "Delete";
-            this.btnDeleteAccomm.UseVisualStyleBackColor = true;
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Location = new System.Drawing.Point(79, 2);
+            this.cmbType.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(76, 21);
+            this.cmbType.TabIndex = 7;
+            this.cmbType.SelectedIndexChanged += new System.EventHandler(this.cmbType_SelectedIndexChanged);
             // 
             // lblSearchType
             // 
             this.lblSearchType.AutoSize = true;
-            this.lblSearchType.Location = new System.Drawing.Point(3, 25);
+            this.lblSearchType.Location = new System.Drawing.Point(2, 7);
+            this.lblSearchType.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSearchType.Name = "lblSearchType";
-            this.lblSearchType.Size = new System.Drawing.Size(98, 17);
+            this.lblSearchType.Size = new System.Drawing.Size(73, 13);
             this.lblSearchType.TabIndex = 2;
             this.lblSearchType.Text = "Filter by Type:";
             // 
-            // btnDisplayAccomm
+            // pnlDelete
             // 
-            this.btnDisplayAccomm.Location = new System.Drawing.Point(267, 27);
-            this.btnDisplayAccomm.Name = "btnDisplayAccomm";
-            this.btnDisplayAccomm.Size = new System.Drawing.Size(78, 58);
-            this.btnDisplayAccomm.TabIndex = 0;
-            this.btnDisplayAccomm.Text = "Display";
-            this.btnDisplayAccomm.UseVisualStyleBackColor = true;
+            this.pnlDelete.BackColor = System.Drawing.Color.Silver;
+            this.pnlDelete.Controls.Add(this.txtNrOcc);
+            this.pnlDelete.Controls.Add(this.txtType);
+            this.pnlDelete.Controls.Add(this.txtAccNr);
+            this.pnlDelete.Controls.Add(this.lblNrOcc);
+            this.pnlDelete.Controls.Add(this.lblAccType);
+            this.pnlDelete.Controls.Add(this.btnDeleteAccomm);
+            this.pnlDelete.Controls.Add(this.lblAccNr);
+            this.pnlDelete.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pnlDelete.Location = new System.Drawing.Point(21, 21);
+            this.pnlDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlDelete.Name = "pnlDelete";
+            this.pnlDelete.Size = new System.Drawing.Size(222, 363);
+            this.pnlDelete.TabIndex = 19;
             // 
-            // panel4
+            // txtNrOcc
             // 
-            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtNrOcc.Enabled = false;
+            this.txtNrOcc.Location = new System.Drawing.Point(124, 66);
+            this.txtNrOcc.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNrOcc.Name = "txtNrOcc";
+            this.txtNrOcc.Size = new System.Drawing.Size(76, 20);
+            this.txtNrOcc.TabIndex = 10;
+            // 
+            // txtType
+            // 
+            this.txtType.Enabled = false;
+            this.txtType.Location = new System.Drawing.Point(124, 36);
+            this.txtType.Margin = new System.Windows.Forms.Padding(2);
+            this.txtType.Name = "txtType";
+            this.txtType.Size = new System.Drawing.Size(76, 20);
+            this.txtType.TabIndex = 9;
+            // 
+            // txtAccNr
+            // 
+            this.txtAccNr.Enabled = false;
+            this.txtAccNr.Location = new System.Drawing.Point(124, 6);
+            this.txtAccNr.Margin = new System.Windows.Forms.Padding(2);
+            this.txtAccNr.Name = "txtAccNr";
+            this.txtAccNr.Size = new System.Drawing.Size(76, 20);
+            this.txtAccNr.TabIndex = 8;
+            // 
+            // lblNrOcc
+            // 
+            this.lblNrOcc.AutoSize = true;
+            this.lblNrOcc.Location = new System.Drawing.Point(4, 68);
+            this.lblNrOcc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNrOcc.Name = "lblNrOcc";
+            this.lblNrOcc.Size = new System.Drawing.Size(109, 13);
+            this.lblNrOcc.TabIndex = 7;
+            this.lblNrOcc.Text = "Number of occupants";
+            // 
+            // lblAccType
+            // 
+            this.lblAccType.AutoSize = true;
+            this.lblAccType.Location = new System.Drawing.Point(81, 36);
+            this.lblAccType.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAccType.Name = "lblAccType";
+            this.lblAccType.Size = new System.Drawing.Size(31, 13);
+            this.lblAccType.TabIndex = 6;
+            this.lblAccType.Text = "Type";
+            // 
+            // btnDeleteAccomm
+            // 
+            this.btnDeleteAccomm.Location = new System.Drawing.Point(6, 112);
+            this.btnDeleteAccomm.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDeleteAccomm.Name = "btnDeleteAccomm";
+            this.btnDeleteAccomm.Size = new System.Drawing.Size(194, 47);
+            this.btnDeleteAccomm.TabIndex = 4;
+            this.btnDeleteAccomm.Text = "Delete";
+            this.btnDeleteAccomm.UseVisualStyleBackColor = true;
+            this.btnDeleteAccomm.Click += new System.EventHandler(this.btnDeleteAccomm_Click);
+            // 
+            // lblAccNr
+            // 
+            this.lblAccNr.AutoSize = true;
+            this.lblAccNr.Location = new System.Drawing.Point(22, 11);
+            this.lblAccNr.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblAccNr.Name = "lblAccNr";
+            this.lblAccNr.Size = new System.Drawing.Size(91, 13);
+            this.lblAccNr.TabIndex = 5;
+            this.lblAccNr.Text = "Accommdation Nr";
+            // 
+            // pnlGrid
+            // 
+            this.pnlGrid.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel4.Controls.Add(this.dgView);
-            this.panel4.Location = new System.Drawing.Point(353, 137);
-            this.panel4.Margin = new System.Windows.Forms.Padding(4);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(714, 336);
-            this.panel4.TabIndex = 18;
+            this.pnlGrid.Controls.Add(this.dgView);
+            this.pnlGrid.Location = new System.Drawing.Point(265, 45);
+            this.pnlGrid.Name = "pnlGrid";
+            this.pnlGrid.Size = new System.Drawing.Size(536, 339);
+            this.pnlGrid.TabIndex = 18;
             // 
             // dgView
             // 
@@ -138,105 +198,30 @@ namespace vaalrusGUIPrototype.Forms
             this.dgView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.dgView.Location = new System.Drawing.Point(0, 0);
-            this.dgView.Margin = new System.Windows.Forms.Padding(4);
             this.dgView.Name = "dgView";
             this.dgView.ReadOnly = true;
             this.dgView.RowHeadersWidth = 51;
-            this.dgView.Size = new System.Drawing.Size(714, 336);
+            this.dgView.Size = new System.Drawing.Size(536, 339);
             this.dgView.TabIndex = 17;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(143, 22);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(100, 24);
-            this.comboBox1.TabIndex = 7;
-            // 
-            // panel2
-            // 
-            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.panel2.Controls.Add(this.textBox1);
-            this.panel2.Controls.Add(this.lblSearchNumber);
-            this.panel2.Controls.Add(this.comboBox1);
-            this.panel2.Controls.Add(this.lblSearchType);
-            this.panel2.Controls.Add(this.btnDisplayAccomm);
-            this.panel2.Location = new System.Drawing.Point(353, 26);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(714, 100);
-            this.panel2.TabIndex = 20;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(143, 63);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 8;
-            // 
-            // lblAccNr
-            // 
-            this.lblAccNr.AutoSize = true;
-            this.lblAccNr.Location = new System.Drawing.Point(29, 13);
-            this.lblAccNr.Name = "lblAccNr";
-            this.lblAccNr.Size = new System.Drawing.Size(119, 17);
-            this.lblAccNr.TabIndex = 5;
-            this.lblAccNr.Text = "Accommdation Nr";
-            // 
-            // lblAccType
-            // 
-            this.lblAccType.AutoSize = true;
-            this.lblAccType.Location = new System.Drawing.Point(108, 44);
-            this.lblAccType.Name = "lblAccType";
-            this.lblAccType.Size = new System.Drawing.Size(40, 17);
-            this.lblAccType.TabIndex = 6;
-            this.lblAccType.Text = "Type";
-            // 
-            // lblNrOcc
-            // 
-            this.lblNrOcc.AutoSize = true;
-            this.lblNrOcc.Location = new System.Drawing.Point(5, 84);
-            this.lblNrOcc.Name = "lblNrOcc";
-            this.lblNrOcc.Size = new System.Drawing.Size(143, 17);
-            this.lblNrOcc.TabIndex = 7;
-            this.lblNrOcc.Text = "Number of occupants";
-            // 
-            // txtAccNr
-            // 
-            this.txtAccNr.Location = new System.Drawing.Point(166, 8);
-            this.txtAccNr.Name = "txtAccNr";
-            this.txtAccNr.Size = new System.Drawing.Size(100, 22);
-            this.txtAccNr.TabIndex = 8;
-            // 
-            // txtType
-            // 
-            this.txtType.Location = new System.Drawing.Point(166, 44);
-            this.txtType.Name = "txtType";
-            this.txtType.Size = new System.Drawing.Size(100, 22);
-            this.txtType.TabIndex = 9;
-            // 
-            // txtNrOcc
-            // 
-            this.txtNrOcc.Location = new System.Drawing.Point(166, 81);
-            this.txtNrOcc.Name = "txtNrOcc";
-            this.txtNrOcc.Size = new System.Drawing.Size(100, 22);
-            this.txtNrOcc.TabIndex = 10;
+            this.dgView.SelectionChanged += new System.EventHandler(this.dgView_SelectionChanged);
             // 
             // frmDeleteAccommodation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1090, 553);
+            this.ClientSize = new System.Drawing.Size(818, 449);
             this.Controls.Add(this.panelChildForm);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmDeleteAccommodation";
             this.Text = "frmDeleteAccommodation_1_";
+            this.Load += new System.EventHandler(this.frmDeleteAccommodation_Load_1);
             this.panelChildForm.ResumeLayout(false);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.panel4.ResumeLayout(false);
+            this.pnlFilter.ResumeLayout(false);
+            this.pnlFilter.PerformLayout();
+            this.pnlDelete.ResumeLayout(false);
+            this.pnlDelete.PerformLayout();
+            this.pnlGrid.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgView)).EndInit();
-            this.panel2.ResumeLayout(false);
-            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -244,16 +229,13 @@ namespace vaalrusGUIPrototype.Forms
         #endregion
 
         private System.Windows.Forms.Panel panelChildForm;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel pnlGrid;
         private System.Windows.Forms.DataGridView dgView;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label lblSearchNumber;
+        private System.Windows.Forms.Panel pnlDelete;
         private System.Windows.Forms.Button btnDeleteAccomm;
         private System.Windows.Forms.Label lblSearchType;
-        private System.Windows.Forms.Button btnDisplayAccomm;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel pnlFilter;
+        private System.Windows.Forms.ComboBox cmbType;
         private System.Windows.Forms.TextBox txtNrOcc;
         private System.Windows.Forms.TextBox txtType;
         private System.Windows.Forms.TextBox txtAccNr;
