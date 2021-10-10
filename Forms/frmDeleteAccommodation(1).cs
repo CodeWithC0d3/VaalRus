ba38d634a5cdfa76179fb997f28473fd2f9f5b1c
WLHeyneke <69819966+WLHeyneke@users.noreply.github.com>
2021-10-10 16:47:38 +0200

    Del Accomm form update

---
+++ Forms/frmDeleteAccommodation(1).cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vaalrusGUIPrototype.Forms
+{
+    public partial class frmDeleteAccommodation_1_ : Form
+    {
+        public frmDeleteAccommodation_1_()
+        {
+            InitializeComponent();
+        }
+    }
+}
