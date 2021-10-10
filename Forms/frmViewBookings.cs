098dd37923320142861ad7c2a6756868d67e107a
WLHeyneke <69819966+WLHeyneke@users.noreply.github.com>
2021-10-10 09:23:20 +0200

    View Bookings DBcon

--- Forms/frmViewBookings.cs
+++ Forms/frmViewBookings.cs
@@ -7,11 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace vaalrusGUIPrototype.Forms
 {
     public partial class frmViewBookings : Form
     {
+        string constr = Properties.Settings.Default.conString;
+        SqlConnection con;
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        DataSet ds;
+
         public frmViewBookings()
         {
             InitializeComponent();
